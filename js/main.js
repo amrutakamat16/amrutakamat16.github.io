2bcf3572732300ed56a21eeed8ebc2adec464f30
amrutakamat16 <amruta.kamat16@gmail.com>
2017-02-20 22:18:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   
   $(".post-header a").removeAttr("href")
   
-  $(".post .post-header a").first().css("font-size": "-webkit-xxx-large")
+  $(".post .post-header a").first().css("font-size", "-webkit-xxx-large")
+  $(".post .post-meta").first().css("display", "none")
 });
